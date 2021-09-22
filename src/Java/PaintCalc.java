1194f8a0157e89ee9b2a0b70f3fe17777b0fb23f
Lee Waugh <lwaugh@softwareinstitute.com>
2021-09-22 13:29:23 +0100

    methods and stuff

--- src/Java/PaintCalc.java
+++ src/Java/PaintCalc.java
@@ -4,31 +4,6 @@ import java.util.Scanner;
 
 public class PaintCalc {
 
-    static void paint(double width, double length, double height, int windows, int doors, int coats, String userInput, int coverage) {
-        switch (userInput) {
-            case "wall":
-                double A = height * length;
-                double windowsArea = 0.6 * 0.63 * windows;
-                double doorsArea = 1.981 * 0.762 * doors;
-                double paint = coats * (A - windowsArea - doorsArea) / coverage;
-                paint = Math.round(paint);
-                System.out.println("Paint Required");
-                System.out.println(paint + " " + "litres");
-                break;
-
-            case "regular":
-                double P = 2 * width + 2 * length;
-                A = P * height;
-                windowsArea = 0.6 * 0.63 * windows;
-                doorsArea = 1.981 * 0.762 * doors;
-                paint = coats * (A - windowsArea - doorsArea) / coverage;
-                paint = Math.round(paint);
-                System.out.println("Paint Required");
-                System.out.println(paint + " " + "litres");
-                break;
-                }
-        }
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] colours = {14, 7, 2, 30};
@@ -55,7 +30,7 @@ public class PaintCalc {
             case "room":
                 System.out.println("regular or irregular room?");
                 userInput = scan.nextLine();
-                switch (userInput){
+                switch (userInput) {
                     case "regular":
                         System.out.println("Height of Room (m)?");
                         double height = scan.nextDouble();
@@ -69,38 +44,31 @@ public class PaintCalc {
                         int windows = scan.nextInt();
                         System.out.println("How many Doors?");
                         int doors = scan.nextInt();
-                        paint(width, length, height, windows, doors, coats, userInput, coverage);
+                        System.out.println("Paint Required");
+                        System.out.println(calcRoom(width, length, height, windows, doors, coats, coverage) + " litres");
                         break;
 
                     case "irregular":
-                        height = 0;
                         width = 0;
-                        length = 0;
-                        windows = 0;
-                        doors =0;
                         double total = 0;
                         System.out.println("Number of Coats?");
                         coats = scan.nextInt();
                         System.out.println("How many Walls?");
                         int n = scan.nextInt();
-                        for(int i = 1; i <= n; i++) {
-                            System.out.println("Height of Wall" + " " + i + "(m)?");
+                        for (int i = 1; i <= n; i++) {
+                            System.out.println("Height of Wall " + i + " (m)?");
                             height = scan.nextDouble();
-                            System.out.println("Length of Wall" + " " + i + "(m)?");
+                            System.out.println("Length of Wall " + i + " (m)?");
                             length = scan.nextDouble();
-                            System.out.println("How many Windows on Wall" + " " + i + "?");
+                            System.out.println("How many Windows on Wall " + i + "?");
                             windows = scan.nextInt();
-                            System.out.println("How many Doors on Wall" + " " + i + "?");
+                            System.out.println("How many Doors on Wall " + i + "?");
                             doors = scan.nextInt();
-                            double A = height * length;
-                            double windowsArea = 0.6 * 0.63 * windows;
-                            double doorsArea = 1.981 * 0.762 * doors;
-                            double paint = coats * (A - windowsArea - doorsArea) / coverage;
-                            total = total + paint;
-                            }
-                            System.out.println("Paint Required");
-                            System.out.println(Math.round(total) + " " + "litres");
-                        paint(width, length, height, windows, doors, coats, userInput, coverage);
+                            total = total + calcRoom(width, length, height, windows, doors, coats, coverage);
+                        }
+                        System.out.println("Paint Required");
+                        System.out.println(total + " litres");
+
                         break;
                 }
                 break;
@@ -115,10 +83,36 @@ public class PaintCalc {
                 int windows = scan.nextInt();
                 System.out.println("How many Doors?");
                 int doors = scan.nextInt();
-                paint(0, length, height, windows, doors, coats, userInput, coverage);
+                System.out.println("Paint Required");
+                System.out.println(calcWall(length, height, windows, doors, coats, coverage) + " litres");
                 break;
         }
+
     }
 
+    public static double calcRoom(double width, double length, double height, int windows, int doors, int coats, int coverage) {
+
+        double P = 2 * width + 2 * length;
+        double A = P * height;
+        double windowsArea = 0.6 * 0.63 * windows;
+        double doorsArea = 1.981 * 0.762 * doors;
+        double paint = coats * (A - windowsArea - doorsArea) / coverage;
+        paint = Math.ceil(paint);
+
+        return paint;
+    }
+
+    public static double calcWall(double length, double height, int windows, int doors, int coats, int coverage) {
+
+        double A = height * length;
+        double windowsArea = 0.6 * 0.63 * windows;
+        double doorsArea = 1.981 * 0.762 * doors;
+        double paint = coats * (A - windowsArea - doorsArea) / coverage;
+        paint = Math.ceil(paint);
+
+        return paint;
+
+
+    }
 }
 
